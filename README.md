715e3fd30751d41ef508a19eb5cd842374d36306
Anthony Ngo <anthonyngo002@gmail.com>
2020-08-26 10:10:12 -0700

    test docs

---
+++ README.md
@@ -0,0 +1,35 @@
+<h1 align="center">Welcome to uncalc 👋</h1>
+<p>
+  <img alt="Version" src="https://img.shields.io/badge/version-0.1-blue.svg?cacheSeconds=2592000" />
+  <a href="#" target="_blank">
+    <img alt="License: MIT" src="https://img.shields.io/badge/License-MIT-yellow.svg" />
+  </a>
+</p>
+
+> A simple calculator made in 2 hours
+
+### 🏠 [Homepage](https://github.com/ngoantho/uncalc)
+
+## Install
+
+```sh
+yarn install
+```
+
+## Usage
+
+```sh
+yarn run start
+```
+
+## Author
+
+👤 **Anthony Ngo**
+
+- Website: anthonyngo.me
+- Github: [@ngoantho](https://github.com/ngoantho)
+- LinkedIn: [@anthongo](https://linkedin.com/in/anthongo)
+
+## Show your support
+
+Give a ⭐️ if this project helped you!
---
+++ docs/precache-manifest.93ade198cc4ffc5b5cc0c994e10d444c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "7cbb4d566d25bd34260d",
+    "url": "/static/js/app.d75a6763.chunk.js"
+  },
+  {
+    "revision": "66090dc4495d4ce3dfb9",
+    "url": "/static/js/runtime~app.2e9f1821.js"
+  },
+  {
+    "revision": "d6f016a6c0da71292a22",
+    "url": "/static/js/2.b56472e8.chunk.js"
+  },
+  {
+    "revision": "f328c398676e066fb4e76f0c028592b4",
+    "url": "/expo-service-worker.js"
+  },
+  {
+    "revision": "0def6b905e24c8b6569db2d15679c733",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e802d2644326fd9fa701f1d969a8a94a",
+    "url": "/register-service-worker.js"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "d18dc8d75e0bcba12ba2a8966cb8c872",
+    "url": "/static/js/2.b56472e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71033553ddc0843407efe54c23890fe2",
+    "url": "/favicon-16.png"
+  },
+  {
+    "revision": "c941078418f3ad7bb94a9ca94bc8b3dc",
+    "url": "/favicon-32.png"
+  },
+  {
+    "revision": "f0a41fce6a41913d0750da190a5be32d",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "8a33cdad3ebed70410d84577ee51c627",
+    "url": "/manifest.json"
+  }
+];
